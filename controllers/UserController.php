83292065ce7f920c1b07ef2ef62968d1e371adcd
Witalii Nikitin <witalikalive@gmail.com>
2019-02-17 01:47:33 +0500

    PHP-методы добавлены

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -3,4 +3,59 @@
 class UserController
 {
 
+    // добавляет пользователя
+    public function actionAddUser()
+    {
+
+        $lastname = trim($_POST['lastname']);
+        $firstname = trim($_POST['firstname']);
+        $patronymic = trim($_POST['patronymic']);
+
+        // Присутствуют недопустимые символы
+        if (!User::checkUserName($lastname, $firstname, $patronymic)) {
+
+            echo json_encode(['result' => 'false']);
+            return false;
+        }
+
+        echo json_encode([
+            'lastname' => $lastname,
+            'firstname' => $firstname,
+            'patronymic' => $patronymic
+        ]);
+
+        return true;
+    }
+
+    // удаляет пользователя
+    public function actionDeleteUser()
+    {
+        $id = $_POST['id'];
+
+        $result = User::deleteUser($id);
+
+        if (!$result) {
+            echo json_encode([
+                'result' => 'false'
+            ]);
+
+            return false;
+        }
+
+        echo json_encode([
+            'id' => $id
+        ]);
+
+        return true;
+    }
+
+    // fixme Удалить?
+    // возвращает пользователей
+    public function actionGetUsers()
+    {
+        $users = User::getUsers();
+        echo json_encode($users);
+
+        return true;
+    }
 }
--- models/User.php
+++ models/User.php
@@ -3,4 +3,57 @@
 class User
 {
 
+    // добавляет пользователя
+    public static function addUser($lastname, $firstname, $patronymic)
+    {
+        $db = Db::getConnection();
+
+        $sql = "INSERT INTO user (lastname, firstname, patronymic) VALUES(:lastname, :firstname, :patronymic)";
+
+        $result = $db->prepare($sql);
+        $result->bindParam(':lastname', $lastname, PDO::PARAM_STR);
+        $result->bindParam(':firstname', $firstname, PDO::PARAM_STR);
+        $result->bindParam(':patronymic', $patronymic, PDO::PARAM_STR);
+
+        return $result->execute();
+    }
+
+    // удаляет пользователя
+    public static function deleteUser($id)
+    {
+        $db = Db::getConnection();
+
+        $sql = "DELETE FROM user WHERE id = :id";
+
+        $result = $db->prepare($sql);
+        $result->bindParam(':id', $id, PDO::PARAM_INT);
+
+        return $result->execute();
+    }
+
+    // возвращает пользователей
+    public static function getUsers()
+    {
+        $db = Db::getConnection();
+
+        $sql = "SELECT FROM user ORDER BY id DESC";
+        $result = $db->query($sql);
+
+        $users = $result->fetchAll(PDO::FETCH_ASSOC);
+
+        return $users;
+    }
+
+    public static function checkUserName($lastname, $firstname, $patronymic)
+    {
+        $result = true;
+
+        if (preg_match('/[^[:alpha:]-]/u', $lastname) || preg_match('/[^[:alpha:]-]/u', $firstname) ||
+            preg_match('/[^[:alpha:]-]/u', $patronymic)) {
+
+            $result = false;
+        }
+
+        return $result;
+    }
 }

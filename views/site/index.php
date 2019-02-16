2c230b41c1d5bb6d8d484ad9af3093ab0b07b872
Witalii Nikitin <witalikalive@gmail.com>
2019-02-16 19:11:05 +0500

    Каркас HTML готов

--- views/site/index.php
+++ views/site/index.php
@@ -1,3 +1,19 @@
 <?php include ROOT . '/views/layouts/header.php'; ?>
-
+    <div class="wrapper">
+        <div class="form">
+            <div class="form_field" id="id_form_lastname"></div>
+            <div class="form_field" id="id_form_firstname"></div>
+            <div class="form_field" id="id_form_patronymic"></div>
+            <div class="form_field" id="id_form_add_button"></div>
+        </div>
+        <div class="table">
+            <div class="table_row">
+                <div class="table_ceil"></div>
+                <div class="table_ceil"></div>
+                <div class="table_ceil"></div>
+                <div class="table_ceil"></div>
+                <div class="table_ceil table_ceil_delete_button" id="id_table_row_id"></div>
+            </div>
+        </div>
+    </div>
 <?php include ROOT . '/views/layouts/footer.php'; ?>
